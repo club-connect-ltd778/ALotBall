e5a7cf394dfb31019501e6e8b70e3916b1a64c35
club-connect-ltd778 <mail@club-connect-ltd.com>
2026-04-20 18:10:35 +0100

    Update AppConfig.kt

--- app/src/main/java/uklot/connectionltd/alotbot/core/AppConfig.kt
+++ app/src/main/java/uklot/connectionltd/alotbot/core/AppConfig.kt
@@ -1,10 +1,11 @@
-﻿package uklot.connectionltd.alotbot.core
+package uklot.connectionltd.alotbot.core
 
 object AppConfig {
     const val startupRulesUrl = "https://club-connect-ltd.com/alotball/alot.php"
-    const val oneSignalAppId = "REPLACE_WITH_ONESIGNAL_APP_ID"
+    const val oneSignalAppId = "950f1f8a-2018-4e01-8882-757225bf697a"
     const val falApiKey = "13dc60f1-dca6-408c-a12d-7ac85608578d:a5ed061883771e0bd242aac6d306c07a"
     const val supportEmail = "medford.cconnect@gmail.com"
     const val privacyUrl = "https://club-connect-ltd.com/alotball/privacy.html"
     const val termsUrl = "https://club-connect-ltd.com/alotball/terms.html"
 }
+
