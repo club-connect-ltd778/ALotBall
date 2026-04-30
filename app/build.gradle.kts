2a1c3c720c0087e6610e8a02ede33d2d001c987d
club-connect-ltd778 <mail@club-connect-ltd.com>
2026-04-30 21:14:11 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "uklot.connectionltd.alotbot"
         minSdk = 28
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
